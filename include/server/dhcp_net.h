ce2b978ebe9df499fac16fe4f95a18bc73ac7509
Kalashnik Daniil <ml963852@yandex.ru>
2025-08-27 02:49:12 +0700

    [#1] Adding lib dhcp_net

---
+++ include/server/dhcp_net.h
@@ -0,0 +1,26 @@
+#ifndef DHCP_NET_H
+#define DHCP_NET_H
+
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/un.h>
+
+#define DHCP_NET_ERR -1
+#define DHCP_NET_OK   0
+
+#define IS_ERROR(func) func == DHCP_NET_ERR
+
+int dhcp_net_create_sock(int * fd, int domain, int type);
+int dhcp_net_udp_inet_sock(uint32_t addr, uint16_t port, struct sockaddr_in * serv);
+int dhcp_net_set_reuse_addr(int fd);
+int dhcp_net_udp_local_sock(char * path, struct sockaddr_un * addr);
+int dhcp_net_udp_unix_server(char * path, struct sockaddr_un * addr);
+int dhcp_net_udp_unix_client(char * path, struct sockaddr_un * addr);
+
+#endif /* DHCP_NET_H */
