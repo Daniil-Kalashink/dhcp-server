e9487ac96d48b811e314afb382cd78dfd5185878
Kalashnik Daniil <ml963852@yandex.ru>
2025-08-28 08:12:32 +0700

    Refactoric function main and server_for_receiving
    In this commite, fixed:
     - incorrect names of variables and macros
     - buffer is stuck and now the size is tied to the value of the maximum MTU
     - Removen err_close
     - added define dhcp_mtu_max, dhcp_mtu_min

--- include/server/dhcp.h
+++ include/server/dhcp.h
@@ -7,7 +7,9 @@
 #include <pthread.h>
 #include "include/server/dhcp_net.h"
 
-#define PORT_SERVER 67
+#define DHCP_PORT_SERVER 67
+#define DHCP_MTU_MAX     1500
+#define DHCP_MTU_MIN     576
 
 void *server_for_receiving(void* arg);
 
